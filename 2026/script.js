6168136103493c3ce0685c3c8b17f7057bb3aee4
Raghavendra Challapali <challapali@gmail.com>
2026-02-15 22:45:30 +0530

    Delete 2026 directory

--- 2026/script.js
+++
@@ -1,6 +0,0 @@
-const selector = document.getElementById("monthSelector");
-const image = document.getElementById("calendarImage");
-
-selector.addEventListener("change", function() {
-    image.src = "images/" + this.value;
-});
